0de6db3b437c7a26b8132cf9d7aaa04a4be6c29e
Jonathan Khanlian <jdkhanlian@gmail.com>
2023-05-06 17:48:49 -0400

    adding claw game working files

---
+++ images/claw_game.js
@@ -0,0 +1,89 @@
+// let square = document.getElementById("square");
+// let homePos = { x: 0, y: 0 };
+// let isDropped = false;
+
+// function moveUp() {
+// 	if (!isDropped) {
+// 		square.style.top = parseInt(square.style.top) - 50 + "px";
+// 	}
+// }
+
+// function moveDown() {
+// 	if (!isDropped) {
+// 		square.style.top = parseInt(square.style.top) + 50 + "px";
+// 	}
+// }
+
+// function moveLeft() {
+// 	if (!isDropped) {
+// 		square.style.left = parseInt(square.style.left) - 50 + "px";
+// 	}
+// }
+
+// function moveRight() {
+// 	if (!isDropped) {
+// 		square.style.left = parseInt(square.style.left) + 50 + "px";
+// 	}
+// }
+
+// function drop() {
+// 	if (!isDropped) {
+// 		isDropped = true;
+// 		let currPos = { x: parseInt(square.style.left), y: parseInt(square.style.top) };
+// 		square.style.top = currPos.y + 200 + "px";
+// 		setTimeout(function() {
+// 			square.style.top = currPos.y + "px";
+// 			setTimeout(function() {
+// 				square.style.top = homePos.y + "px";
+// 				square.style.left = homePos.x + "px";
+// 				isDropped = false;
+// 			}, 500);
+// 		}, 500);
+// 	}
+// }
+
+
+let square = document.getElementById("square");
+let homePos = { x: 50, y: 50 };
+let isDropped = false;
+
+function moveUp() {
+	if (!isDropped && parseInt(square.style.top) > 0) {
+		square.style.top = parseInt(square.style.top) - 50 + "px";
+	}
+}
+
+function moveDown() {
+	if (!isDropped && parseInt(square.style.top) < 450) {
+		square.style.top = parseInt(square.style.top) + 50 + "px";
+        // console.log("test square.style", square.style);
+	}
+}
+
+function moveLeft() {
+	if (!isDropped && parseInt(square.style.left) > 0) {
+		square.style.left = parseInt(square.style.left) - 50 + "px";
+	}
+}
+
+function moveRight() {
+	if (!isDropped && parseInt(square.style.left) < 450) {
+		square.style.left = parseInt(square.style.left) + 50 + "px";
+	}
+}
+
+function drop() {
+	if (!isDropped) {
+		isDropped = true;
+		let currPos = { x: parseInt(square.style.left), y: parseInt(square.style.top) };
+		square.style.top = currPos.y + 200 + "px";
+		setTimeout(function() {
+			square.style.top = currPos.y + "px";
+			setTimeout(function() {
+				square.style.top = homePos.y + "px";
+				square.style.left = homePos.x + "px";
+				isDropped = false;
+			}, 500);
+		}, 500);
+	}
+}
