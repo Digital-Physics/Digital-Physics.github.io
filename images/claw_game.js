d6e13887d98918b1dcb1ab2d6ba49790f3281f45
Jonathan Khanlian <jdkhanlian@gmail.com>
2023-05-07 23:24:08 -0400

    updated claw machine

--- images/claw_game.js
+++ images/claw_game.js
@@ -1,89 +1,59 @@
-// let square = document.getElementById("square");
-// let homePos = { x: 0, y: 0 };
-// let isDropped = false;
-
-// function moveUp() {
-// 	if (!isDropped) {
-// 		square.style.top = parseInt(square.style.top) - 50 + "px";
-// 	}
-// }
-
-// function moveDown() {
-// 	if (!isDropped) {
-// 		square.style.top = parseInt(square.style.top) + 50 + "px";
-// 	}
-// }
-
-// function moveLeft() {
-// 	if (!isDropped) {
-// 		square.style.left = parseInt(square.style.left) - 50 + "px";
-// 	}
-// }
-
-// function moveRight() {
-// 	if (!isDropped) {
-// 		square.style.left = parseInt(square.style.left) + 50 + "px";
-// 	}
-// }
-
-// function drop() {
-// 	if (!isDropped) {
-// 		isDropped = true;
-// 		let currPos = { x: parseInt(square.style.left), y: parseInt(square.style.top) };
-// 		square.style.top = currPos.y + 200 + "px";
-// 		setTimeout(function() {
-// 			square.style.top = currPos.y + "px";
-// 			setTimeout(function() {
-// 				square.style.top = homePos.y + "px";
-// 				square.style.left = homePos.x + "px";
-// 				isDropped = false;
-// 			}, 500);
-// 		}, 500);
-// 	}
-// }
-
-
 let square = document.getElementById("square");
 let homePos = { x: 0, y: 0 };
 let isDropped = false;
+// console.log("square check", square.style.left);
 
 function moveUp() {
 	if (!isDropped && parseInt(square.style.top) > 0) {
-		square.style.top = parseInt(square.style.top) - 50 + "px";
+		square.style.top = parseInt(square.style.top) - 1 + "px";
 	}
 }
 
 function moveDown() {
 	if (!isDropped && parseInt(square.style.top) < 450) {
-		square.style.top = parseInt(square.style.top) + 50 + "px";
-        // console.log("test square.style", square.style);
+		square.style.top = parseInt(square.style.top) + 1 + "px";
 	}
 }
 
 function moveLeft() {
 	if (!isDropped && parseInt(square.style.left) > 0) {
-		square.style.left = parseInt(square.style.left) - 50 + "px";
+		square.style.left = parseInt(square.style.left) - 7 + "px";
 	}
 }
 
 function moveRight() {
+    console.log("moved right", square.style.left);
 	if (!isDropped && parseInt(square.style.left) < 450) {
-		square.style.left = parseInt(square.style.left) + 50 + "px";
+		square.style.left = parseInt(square.style.left) + 7 + "px";
 	}
 }
 
 function drop() {
-	if (!isDropped) {
-		isDropped = true;
-		let currPos = { x: parseInt(square.style.left), y: parseInt(square.style.top) };
-		square.style.top = currPos.y + 200 + "px";
-		setTimeout(function() {
-			square.style.top = currPos.y + "px";
-			setTimeout(function() {
-				square.style.top = homePos.y + "px";
-				square.style.left = homePos.x + "px";
-				isDropped = false;
-			}, 500);
-		}, 500);
-	}
+    if (!isDropped) {
+        isDropped = true;
+        let currPos = { x: parseInt(square.style.left), y: parseInt(square.style.top) };
+        let startPos = currPos.y;
+        let startPosX = currPos.x;
+        let i = 0;
+        let max_distance = Math.max(currPos.x, currPos.y);
+        console.log("max dist", max_distance);
+        const interval = setInterval(function() {
+            if (i < 100) {
+                square.style.top = startPos + i + "px";
+            } else if (i < 200) {
+                square.style.top = startPos + (200 - i) + "px";
+            } else if (i < (200 + max_distance)) {
+                square.style.top = Math.max(0, startPos - (i - 200)) + "px";
+                square.style.left = Math.max(0, startPosX - (i - 200)) + "px";
+            } else {
+                square.style.top = "0px";
+                square.style.left = "0px";
+                isDropped = false;
+                clearInterval(interval);
+            }
+            i += 2;
+        }, 50);
+    }
 }
+
+
